eccc77232aa4799b1e565abd32ff05a17b61d588
Andrew Roxby <terabytedragon@gmail.com>
2013-11-05 23:59:01 -0500

    Nightly Commit for 11-05-13

--- www/sandbox/js/uploadHandler.php
+++ www/sandbox/js/uploadHandler.php
@@ -1,6 +1,12 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT'].'/scripts/common.php');
 noCache();
+
+/*
+413 Request Entity Too Large
+415 Unsupported Media Type
+*/
+
 echo "<pre>";
 print_r($_FILES['images']);
 echo "</pre>";
