b1e29f31a71d38bab8afa642e36a54a512342e6d
Andrew Roxby <terabytedragon@gmail.com>
2013-11-07 23:59:02 -0500

    Nightly Commit for 11-07-13

--- www/secrets/missionboundarynew.php
+++ www/secrets/missionboundarynew.php
@@ -1,27 +1,23 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT']."/scripts/db.php");
 
-class Point{
-	public $lat;//x
-	public $lng;//y
-}
-
 function mult($sp,$ep,$op){
-	return ($sp->lat - $op->lat) * ($ep->lng - $op->lng)  
-        >= ($ep->lat - $op->lat) * ($sp->lng - $op->lng); 
+	return ($sp[lat] - $op[lat]) * ($ep[lng] - $op[lng])
+        >= ($ep[lat] - $op[lat]) * ($sp[lng] - $op[lng]);
 }
 
 function cmp($l,$r){
-	return $l->lng < $r->lng || ($l->lng == $r->lng && $l->lat < $r->lat);  
+    if($l[lng] == $r[lng]) return $l[lat] < $r[lat];
+    else return $l[lng] < $r[lng];
 }
 
-    if(1){//$_POST['missionID']!=''){
+    if($_POST['missionID']!=''){
 	$db = connectDB();
 	$result['OK'] = 1;
 	$result['error'] = "No error.";
 	
-	$stmt = $db->prepare("select location.lat, location.lng from location, missionlocation where missionlocation.missionID=1 and missionlocation.locationID=location.id");
-	//$stmt->bind_param('i', $_POST['missionID']);
+	$stmt = $db->prepare("select location.lat, location.lng from location, missionlocation where missionlocation.missionID=? and missionlocation.locationID=location.id");
+	$stmt->bind_param('i', $_POST['missionID']);
 	$stmt->execute();
 	//$stmt->bind_result($lattemp, $lngtemp);
     bind_array($stmt, $row);
@@ -30,19 +26,48 @@ function cmp($l,$r){
         $points[] = copyArray($row);
         $point_NUM ++;
 	}
-    //echo $points[0][lat];
-    //echo $points[5][lat];
 	$stmt->close();
     
     
-    /*
+    
 	usort($points, "cmp");
+    
 	$vertices = array();
 	$top = 1;
+        
+    //echo $point_NUM;
 	
 	if($point_NUM == 0 ){ $result[vertice_NUM] = 0;}
-	else if ( $point_NUM == 1){$result[vertice_NUM] = 1;$vertices[0] = $points[0];}
-	else if ( $point_NUM == 2){$result[vertice_NUM] = 2;$vertices[0] = $points[0];$vertices[1] = $points[1];}
+	else if ( $point_NUM == 1){
+        $result[vertice_NUM] = 4;
+        $vertices[0] = $points[0];
+        $centerpoint = $points[0];
+        $vertices[0][lat] = $points[0][lat]+0.0001;
+        $vertices[0][lng] = $points[0][lng]+0.0001;
+        $vertices[1][lat] = $points[0][lat]+0.0001;
+        $vertices[1][lng] = $points[0][lng]-0.0001;
+        $vertices[2][lat] = $points[0][lat]-0.0001;
+        $vertices[2][lng] = $points[0][lng]-0.0001;
+        $vertices[3][lat] = $points[0][lat]-0.0001;
+        $vertices[3][lng] = $points[0][lng]+0.0001;
+    }
+	else if ($point_NUM == 2){
+        $result[vertice_NUM] = 4;
+        $vertices[0] = $points[0];
+        $vertices[1] = $points[1];
+        $centerlat = ($points[0][lat]+$points[1][lat])/2;
+        $centerlng = ($points[0][lng]+$points[1][lng])/2;
+        $centerpoint[lat] = $centerlat;
+        $centerpoint[lng] = $centerlng;
+        $vertices[0][lat] = $points[0][lat]*1.5 - $centerlat*0.5;
+        $vertices[0][lng] = $points[0][lng]*1.5 - $centerlng*0.5;
+        $vertices[2][lat] = $points[1][lat]*1.5 - $centerlat*0.5;
+        $vertices[2][lng] = $points[1][lng]*1.5 - $centerlng*0.5;
+        $vertices[1][lat] = $vertices[0][lat];
+        $vertices[1][lng] = $vertices[2][lng];
+        $vertices[3][lat] = $vertices[2][lat];
+        $vertices[3][lng] = $vertices[0][lng];
+    }
 	else{
 		$vertices[0] = $points[0];
 		$vertices[1] = $points[1];
@@ -55,17 +80,44 @@ function cmp($l,$r){
 		}
 		$length = $top;
 		$vertices[++$top] = $points[$point_NUM-2];
+        
 		for($i = $point_NUM-3; $i >= 0; $i--){
-			while(top!=length && mult($points[$i], $vertices[top], $vertices[$top-1])){
+			while($top!=$length && mult($points[$i], $vertices[top], $vertices[$top-1])){
 				$top--;				
 			}
 			$vertices[++$top] = $points[$i];
 		}
 		$result[vertice_NUM] = $top;
-	}	
-	for($i = 0;$i < count($vertices);$i++){
-		$result[] = $vertices[$i];
-	}*/
+        //echo $top;
+        //print_r($vertices[0]);
+        //print_r($vertices[1]);
+        //print_r($vertices[2]);
+        
+        $sumlat = 0;
+        $sumlng = 0;
+        for($i = 0;$i < $top; $i++){
+            $sumlat += $vertices[$i][lat];
+            $sumlng += $vertices[$i][lng];
+            //print_r($vertices[$i]);
+        }
+        $sumlat /= $top;
+        $sumlng /= $top;
+        
+        $centerpoint[lat] = $sumlat;
+        $centerpoint[lng] = $sumlng;
+        
+        for($i = 0;$i < $top; $i++){
+            $vertices[$i][lat] = (1.5 * $vertices[$i][lat] - 0.5 * $sumlat);
+            $vertices[$i][lng] = 1.5 * $vertices[$i][lng] - 0.5 * $sumlng;
+            //print_r($vertices[$i]);
+        }
+
+	}
+    $result['center'] = $centerpoint;
+        
+    for($i = 0;$i < $result[vertice_NUM]; $i++){
+		$result['vertices'][$i] = $vertices[$i];
+	}
 }
 else
 {
@@ -73,5 +125,5 @@ else
 	$result['error'] = "Not enough data";
 }
 
-//echo '['.json_encode($result).']';
+echo '['.json_encode($result).']';
 ?>
